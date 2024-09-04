1cd0c531160edbdea420e47966fd0c5c44217e24
Lordkode <richard.ati@epitech.eu>
2024-09-04 10:53:53 +0100

    [UPDATE]: mongodb database settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
+const DB_USER = "eventxAdmin"
+const DB_PASSWORD = encodeURIComponent("eventx2024*#NestJs")
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@eventx.gmopg.mongodb.net/?retryWrites=true&w=majority&appName=eventx`)],
   controllers: [AppController],
   providers: [AppService],
 })
